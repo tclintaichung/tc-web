b9c5cb68f27ea59d170fe38ea4bcb47a2356974a
tclintaichung <tclinnchu@gmail.com>
2025-03-31 11:43:09 +0800

    change dir name

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-import express from 'express';
-import path from 'path';
+import express from "./node_modules/express";
+import path from "./node_modules/path";
 // import posts from "./routes/posts.js";
 import products from "./routes/products.route.js";
 import logger from "./middlewares/logger.js";
 import errorHandler from "./middlewares/error.js";
-import {fileURLToPath} from 'url';
-import mongoose from 'mongoose';
+// import { fileURLToPath } from "url";
+import mongoose from "./node_modules/mongoose";
 
 const app = express();
 const port = process.env.PORT || 5050;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(logger);
-// app.use("/api/posts", posts);
-app.use("/api/products", products);
-app.use(errorHandler);
+function start_server() {
+  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(logger);
+  // app.use("/api/posts", posts);
+  app.use("/api/products", products);
+  app.use(errorHandler);
 
-mongoose.connect("mongodb+srv://tclinnchu:4qrGX6vgrx94Avqw@backenddb.ampnpow.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
-.then(()=>{
-  console.log("Connect to database");
-  app.listen(port, () => console.log(`Server is listening at ${port}..`));
-}).catch(()=>{
-  console.log("Conection is failed!");
-});
+  mongoose
+    .connect(
+      "mongodb+srv://tclinnchu:4qrGX6vgrx94Avqw@backenddb.ampnpow.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+    )
+    .then(() => {
+      console.log("Connect to database");
+      app.listen(port, () => console.log(`Server is listening at ${port}..`));
+    })
+    .catch(() => {
+      console.log("Conection is failed!");
+    });
+}
+
+var myButton = document.querySelector("button");
+myButton.addEventListener("click", start_server());
